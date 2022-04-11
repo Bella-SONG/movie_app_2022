ab47c7e950b370c319432e9dbae2c1ce12e32a6c
Bella-SONG <bellabebettertmr@gmail.com>
2022-04-11 13:15:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<Navigation />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/movie_detail" element={<Detail />} />
+			</Routes>
+		</BrowserRouter>
+	);
+	//BrowserRouter router를 감싸는 부분, BrowserRouter가 전체를 감싸야 한다.
+	//Route는 반드시 Routes로 묶어줘야 한다.
+	//path는 URL을 의미하며, /는 home을 의미한다.
+	//router 라우터라는 것은 특정한 것만 보이게 하는것
+	//<Route path="/movie_detail"이 주소로 이동하면 element={<Detail />} />이 컴포넌트를 불러와라
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; //'react'는 node_moudules안에 있는 react를 React로 사용하겠다.
+import ReactDOM from "react-dom";
+import App from "./App"; //같은 src안에 있는 App.js도 App으로 사용하겠다
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	//ReactDom에다가 render(그려라)
+	<App />, //App컴포넌트를
+	document.getElementById("root") //여기에다가 그려라
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//App.js 는 하나하나의 컴포넌트를 의미
+//index.js는 하나하나를 전체로 묶어주는 wrap같은 역할
